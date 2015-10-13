332c8a40bf707f47f34caac64411de0751168894
Maciek Makowski <maciek.makowski@gmail.com>
2015-10-13 21:52:33 +0100

    added empty sections and a description copied from the web manual

--- cabal-install/Main.hs
+++ cabal-install/Main.hs
@@ -1233,7 +1233,29 @@ manpage pname commands = unlines $
   , "are"
   , ""
   ] ++
-  concatMap commandSynopsisLines commands
+  concatMap commandSynopsisLines commands ++
+  [ ".SH DESCRIPTION"
+  , "Cabal is the standard package system for Haskell software. It helps people to configure, " 
+  , "build and install Haskell software and to distribute it easily to other users and developers."
+  , ""
+  , "The command line " ++ pname ++ " tool (also referred to as cabal-install) helps with "
+  , "installing existing packages and also helps people developing their own packages. "
+  , "It can be used to work with local packages or to install packages from online package archives, "
+  , "including automatically installing dependencies. By default it is configured to use Hackage, "
+  , "which is Haskell’s central package archive that contains thousands of libraries and applications "
+  , "in the Cabal package format."
+  , ".SH OPTIONS"
+  , "TODO"
+  , ".SH COMMANDS"
+  ] ++
+  concatMap commandDetailsLines commands ++
+  [ ".SH BUGS"
+  , "TODO"
+  , ".SH SEE ALSO"
+  , "TODO"
+  , ".SH LICENSE"
+  , "TODO"
+  ]
   where
     commandSynopsisLines (CommandSpec ui _ Visible) =
       [ ".B " ++ pname ++ " " ++ (commandName ui)
@@ -1241,3 +1263,8 @@ manpage pname commands = unlines $
       , ".br"
       ]
     commandSynopsisLines (CommandSpec _ _ Hidden) = []
+    commandDetailsLines (CommandSpec ui _ Visible) =
+      [ ".B " ++ pname ++ " " ++ (commandName ui)
+      , "TODO"
+      ]
+    commandDetailsLines (CommandSpec _ _ Hidden) = []
